ac9c47c1eda21039ca4c10334d1a12cddb4c4047
maainul <mainul080@yahoo.com>
2024-02-19 04:58:30 +0600

    Reg and login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AuthLandingPage from './pages/AuthLandingPage';
 import SigninPage from './pages/SigninPage';
 import SignupPage from './pages/SignupPage';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<AuthLandingPage />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </>
   )
---
+++ src/pages/ForgotPassword.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom"
+
+
+const ForgotPassword = () => {
+
+    const navigate = useNavigate()
+
+
+    return (
+        <>
+            <div className="md:bg-blue-600 min-h-screen flex justify-center items-center bg-slate-200">
+                <div className='container bg-white rounded-md max-w-md px-4 py-8 md:px-8 mx-7'>
+                    <div className="text-center text-base md:text-2xl font-bold text-slate-500 mb-4 border-b border-gray-200 pb-3">Forgot Password ? 🔒</div>
+                    <p className='text-sm pb-4 md:text-base text-slate-500'>Your new password must be different from previously used passwords</p>
+                    <form>
+                        <div className='input-details text-gray-500 text-sm md:text-lg'>
+                            <div className='input-box  flex flex-col mb-4'>
+                                <label className='font-medium mb-2'>Email</label>
+                                <input
+                                    type='text'
+                                    placeholder='Enter your email'
+                                    className='border border-slate-300 rounded-md shadow-sm placeholder-slate-400 p-2 focus:outline-none focus:border-sky-400' />
+                            </div>
+
+                        </div>
+                        <div className="inputBtn">
+                            <button
+                                type="submit"
+                                className='w-full bg-blue-600 p-2 text-white rounded-md shadow-sm text-sm md:text-base mt-6'>Login</button>
+                        </div>
+                        <div className='text-center my-4 text-sm md:text-base'>
+                            <span className='text-slate-500'>New on our platform?</span> <span onClick={() => {
+                                navigate('/signup')
+                            }} className='text-indigo-600'>Create an account</span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ForgotPassword
