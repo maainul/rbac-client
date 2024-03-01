0d97e80042f952c363195c1876dd9d57af8a44af
maainul <mainul080@yahoo.com>
2024-03-01 17:20:59 +0600

    Fix Design

--- src/pages/AuthDashboard.js
+++ src/pages/AuthDashboard.js
@@ -9,31 +9,45 @@ const AuthDashboard = () => {
             <div className='h-[90vh] w-[90vw] flex justify-center items-center'>
                 <div className=' p-8 rounded-lg flex flex-col gap-5'>
                     <h1 className='text-3xl font-bold text-center mb-6'>Welcome Back.</h1>
-                    <div className='flex flex-row gap-3 align-middle bg-gray-100 rounded-full p-4 font-semibold  w-72 hover:bg-red-500 hover:text-white hover:cursor-pointer pl-8'>
+                    <div className='flex flex-row gap-3 align-middle bg-gray-100 rounded-full p-4 font-semibold  w-72 hover:bg-red-600 hover:text-white hover:cursor-pointer pl-8'>
                         <FontAwesomeIcon
                             icon={faGoogle}
-                            style={{ marginTop: '3px', color: 'red', fontSize: '20px' }}
-                            className="transition-colors duration-300 ease-in-out hover:text-white" />
+                            className="mt-1 text-red-600 text-xl" />
                         Continue with Google
                     </div>
-                    <div className='flex flex-row gap-3 align-middle bg-gray-100 rounded-full p-4 font-semibold  w-72 hover:bg-blue-500 hover:text-white hover:cursor-pointer pl-8'>
-                        <FontAwesomeIcon icon={faFacebook} style={{ marginTop: '3px', color: '#1877F2', fontSize: '20px' }} />
+                    <div className='flex flex-row gap-3 align-middle bg-gray-100 rounded-full p-4 font-semibold  w-72 hover:bg-blue-600 hover:text-white hover:cursor-pointer pl-8'>
+                        <FontAwesomeIcon
+                            icon={faFacebook}
+                            className="mt-1 text-blue-600 text-xl"
+                        />
                         Continue with Facebook
                     </div>
-                    <div className='flex flex-row gap-3 align-middle bg-gray-100 rounded-full p-4 font-semibold  w-72 hover:bg-blue-600 hover:text-white hover:cursor-pointer pl-8'>
-                        <FontAwesomeIcon icon={faTwitter} style={{ marginTop: '3px', color: '#1DA1F2', fontSize: '20px' }} />
+                    <div className='flex flex-row gap-3 align-middle bg-gray-100 rounded-full p-4 font-semibold  w-72 hover:bg-blue-500 hover:text-white hover:cursor-pointer pl-8'>
+                        <FontAwesomeIcon
+                            icon={faTwitter}
+                            className="mt-1 text-blue-500 text-xl"
+                        />
                         Continue with Twitter
                     </div>
                     <div className='flex flex-row gap-3 align-middle bg-gray-100 rounded-full p-4 font-semibold  w-72 hover:bg-blue-700 hover:text-white hover:cursor-pointer pl-8'>
-                        <FontAwesomeIcon icon={faLinkedin} style={{ marginTop: '3px', color: '#0A66C2', fontSize: '20px' }} />
+                        <FontAwesomeIcon
+                            icon={faLinkedin}
+                            className="mt-1 text-blue-700 text-xl"
+                        />
                         Continue With Linkedin
                     </div>
                     <div className='flex flex-row gap-3 align-middle bg-gray-100 rounded-full p-4 font-semibold  w-72 hover:bg-black hover:text-white hover:cursor-pointer pl-8'>
-                        <FontAwesomeIcon icon={faGithub} style={{ marginTop: '2px', color: '#181717', fontSize: '20px' }} />
+                        <FontAwesomeIcon
+                            icon={faGithub}
+                            className="mt-1 text-black text-xl"
+                        />
                         Continue With Github
                     </div>
                     <div className='flex flex-row gap-3 align-middle bg-gray-100 rounded-full p-4 font-semibold  w-72 hover:bg-black hover:text-white hover:cursor-pointer pl-8'>
-                        <FontAwesomeIcon icon={faApple} style={{ marginTop: '1px', color: '#000000', fontSize: '20px' }} />
+                        <FontAwesomeIcon
+                            icon={faApple}
+                            className="mt-1 text-black text-xl"
+                        />
                         Continue With Apple
                     </div>
                     <div className='text-center'>
