e824cb269a07505401439216f41c28c60d170e3f
maainul <mainul080@yahoo.com>
2024-03-10 20:52:53 +0600

    Update Tabl Dataa

--- src/components/layout/Table.js
+++ src/components/layout/Table.js
@@ -1,82 +1,123 @@
-const Table = () =>{
-    return(
-        
-
-      <div>
-        {/* <h1 className="p-3 text-green-500 font-bold text-lg">User List</h1> */}
-         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-white uppercase bg-blue-500 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Product name
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Color
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Action
-                </th>
-            </tr>
-          </thead>
-         <tbody>
-            <tr class=" border-b dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-                <td class="px-6 py-4 flex gap-5">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
-                </td>
-            </tr>
-            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-                <td class="px-6 py-4 flex gap-5">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
-                </td>
-            </tr>
-           
-          </tbody>
-      </table>
-     </div>
-         )
+const Table = () => {
+    return (
+        <div>
+            {/* <h1 classNameName="p-3 text-green-500 font-bold text-lg">User List</h1> */}
+            <table className="w-full text-sm text-left rtl:text-right text-gray-400">
+                <thead className="text-xs text-white uppercase bg-blue-500">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Product name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Color
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Category
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Category
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="border-b border-gray-300">
+                        <th scope="row" className="font-medium whitespace-nowrap text-gray-400 px-6 py-4">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td className="px-6 py-4">
+                            Silver
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            $2999
+                        </td>
+                        <td className="px-6 py-4 flex gap-5">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                        </td>
+                    </tr>
+                    <tr className=" border-b border-gray-300 ">
+                        <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap text-graay-400">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td className="px-6 py-4">
+                            Silver
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            $2999
+                        </td>
+                        <td className="px-6 py-4 flex gap-5">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                        </td>
+                    </tr>
+                    <tr className=" border-b border-gray-300 ">
+                        <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap text-graay-400">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td className="px-6 py-4">
+                            Silver
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            $2999
+                        </td>
+                        <td className="px-6 py-4 flex gap-5">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                        </td>
+                    </tr>
+                    <tr className=" border-b border-gray-300 ">
+                        <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap text-graay-400">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td className="px-6 py-4">
+                            Silver
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            $2999
+                        </td>
+                        <td className="px-6 py-4 flex gap-5">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Table
