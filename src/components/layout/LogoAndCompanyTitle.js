64acddf7a02e54c7f3b98b105a3eee5769573286
maainul <mainul080@yahoo.com>
2024-03-16 08:38:38 +0600

    Update

--- src/components/layout/LogoAndCompanyTitle.js
+++ src/components/layout/LogoAndCompanyTitle.js
@@ -3,6 +3,7 @@ import logo from "../../logo512.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 const LogoAndCompanyTitle = () => {
+    console.log(logo)
     return (
         <>
             <div className="px-5 py-6 cursor-pointer items-center border-b-2 border-b-gray-200 justify-between flex gap-2 mb-6 bg-white">
